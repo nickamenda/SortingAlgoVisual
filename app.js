aeee4d234a8abe404a3a752ca5baac3eff11d3b8
unknown <nkmorgan@iastate.edu>
2022-07-06 00:59:22 -0500

    got all the buttons and the input text done

--- app.js
+++ app.js
@@ -1,14 +1,62 @@
 'use strict';
 
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+const BUTTON_COLOR = "#7FFF00"
+const DISABLED_BUTTON_COLOR = "#A9A9A9"
 
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
+
+function makeArray() {
+    var numValue= document.getElementById("numValues").value;
+    if (isNaN(numValue)) {
+        window.alert("Input has to be a number!");
     }
-});
+    else if (numValue < 5 || numValue > 1000) {
+    window.alert("Value has to be between 5 and 1000!");
+    }
+    else{
+    window.alert("Nice");
+    }
+}
+
+/**
+ * this function enables all buttons and switches its color to green
+ */
+ function enable() {
+
+    document.getElementById("makeArrayButton").disabled = false;
+    document.getElementById("makeArrayButton").style.backgroundColor = BUTTON_COLOR;
+
+    document.getElementById("quickSortButton").disabled = false;
+    document.getElementById("quickSortButton").style.backgroundColor = BUTTON_COLOR;
+
+    document.getElementById("mergeSortButton").disabled = false;
+    document.getElementById("mergeSortButton").style.backgroundColor = BUTTON_COLOR;
+
+    document.getElementById("insertionSortButton").disabled = false;
+    document.getElementById("insertionSortButton").style.backgroundColor = BUTTON_COLOR;
+
+    document.getElementById("bubbleSortButton").disabled = false;
+    document.getElementById("bubbleSortButton").style.backgroundColor = BUTTON_COLOR;
+
+}
+
+/**
+ * this function disables all buttons and switches its color to gray
+ */
+function disable() {
+
+    document.getElementById("makeArrayButton").disabled = true;
+    document.getElementById("makeArrayButton").style.backgroundColor = DISABLED_BUTTON_COLOR;
+
+    document.getElementById("quickSortButton").disabled = true;
+    document.getElementById("quickSortButton").style.backgroundColor = DISABLED_BUTTON_COLOR;
+
+    document.getElementById("mergeSortButton").disabled = true;
+    document.getElementById("mergeSortButton").style.backgroundColor = DISABLED_BUTTON_COLOR;
+
+    document.getElementById("insertionSortButton").disabled = true;
+    document.getElementById("insertionSortButton").style.backgroundColor = DISABLED_BUTTON_COLOR;
+
+    document.getElementById("bubbleSortButton").disabled = true;
+    document.getElementById("bubbleSortButton").style.backgroundColor = DISABLED_BUTTON_COLOR;
+
+}
