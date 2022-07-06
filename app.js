691135c4b29abacf4615c7b055789800983197a5
unknown <nkmorgan@iastate.edu>
2022-07-06 17:36:36 -0500

    got arrays done

--- app.js
+++ app.js
@@ -2,19 +2,47 @@
 
 const BUTTON_COLOR = "#7FFF00"
 const DISABLED_BUTTON_COLOR = "#A9A9A9"
-
+const container = document.querySelector(".data-section");
 
 function makeArray() {
+
+    deleteArray();
+
     var numValue= document.getElementById("numValues").value;
+    let array = []
+    //checks if input is a number and in range
     if (isNaN(numValue)) {
         window.alert("Input has to be a number!");
     }
-    else if (numValue < 5 || numValue > 1000) {
-    window.alert("Value has to be between 5 and 1000!");
+    else if (numValue < 5 || numValue > 100) {
+    window.alert("Value has to be between 5 and 100!");
     }
+
     else{
-    window.alert("Nice");
+        for(let i = 0; i < numValue; i++) {
+            const value = Math.floor(Math.random() * 100);
+            const bar = document.createElement("div");
+
+            //bar height
+            bar.style.height = '${value*3}px';
+            //translates at x-axis
+            bar.style.transform = `translateX(${i * 30}px)`;
+
+            const label = document.createElement("label");
+            label.classList.add("id_bar");
+            label.innerHTML = value;
+            bar.appendChild(label);
+            container.appendChild(bar);
+            
+        }
     }
+
+
+}
+
+function deleteArray() {
+    const bar = document.querySelector("#bars");
+    bar.innerHTML = '';
 }
 
 /**
